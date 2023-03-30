537fd4f2d233a561f88870558e3ce7c899cfbcf9
Martin Regner Larsen <martin@pnml.dk>
2023-03-30 10:19:56 +0200

    Add zsh config

---
+++ dot_oh-my-zsh/custom/alias.zsh
@@ -0,0 +1,1 @@
+alias pwsh="/mnt/c/Users/dkMaReLa/dev/scoop/apps/powershell/current/pwsh.exe -NoLogo -WorkingDirectory C:/Users/dkMaReLa"
---
+++ dot_oh-my-zsh/custom/themes/symlink_spaceship.zsh-theme
@@ -0,0 +1,1 @@
+/home/dkmarela/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme
