33a8ed3ee7f43bf5facf027479fa674c34db9939
Martin Regner Larsen <martin@pnml.dk>
2023-04-20 08:19:08 +0200

    Add usefull keymaps for 'i' and 'dd'

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,21 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "jj", "<ESC>", { silent = true, noremap = true })
+
+-- Don't put empty lines in default register when using dd
+vim.keymap.set("n", "dd", function()
+  if vim.api.nvim_get_current_line():match("^%s*$") then
+    return '"_dd'
+  else
+    return "dd"
+  end
+end, { silent = true, noremap = true, expr = true })
+
+-- Try to indent properly on newlines
+vim.keymap.set("n", "i", function()
+  if #vim.fn.getline(".") == 0 then
+    return [["_cc]]
+  else
+    return "i"
+  end
+end, { silent = true, noremap = true, expr = true })
